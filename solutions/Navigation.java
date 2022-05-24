063b8be4e3b94ae7bb1c76b5770d820f318cc06d
ngqiaofang <93829712+ngqiaofang@users.noreply.github.com>
2022-05-25 00:28:39 +0800

    Update Navigation.java

--- solutions/Navigation.java
+++ solutions/Navigation.java
@@ -55,9 +55,11 @@ class Navigation {
         }
 
         // Print the shortest path
-        for (int i = path.size() - 1; i >= 1; i--) 
-            System.out.print(path.get(i) + " -> ");
-        System.out.println(path.get(0));
+        for (int i = path.size() - 1; i >= 0; i--) {
+	    if (i > 0)
+            	System.out.print(path.get(i) + " -> ");
+	    else
+       	 	System.out.println(path.get(0));
     }
  
     // BFS that stores predecessor of each vertex in array pred[]
