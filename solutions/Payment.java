d032bbcc844fc593fdf750e4c32772ec6bffe5d5
CopperCleric <92703867+CopperCleric@users.noreply.github.com>
2022-05-25 15:11:51 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -62,7 +62,7 @@ public class Payment {
                                 
                             }
                         }
-                        break
+                        break;
                     }
                     
                     
