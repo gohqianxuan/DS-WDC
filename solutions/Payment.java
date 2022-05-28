b241a4c2830ca3e718ff47e8dca91516fe52c618
CopperCleric <92703867+CopperCleric@users.noreply.github.com>
2022-05-29 03:19:48 +0800

    Add files via upload

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,252 +1,116 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
+import java.util.PriorityQueue;
 
 public class Payment {
-    static Long time;
-    static String id , tier;
-    static TransactionDetail trans;
-    static int index1 = 0, index2 , index3 = 0;
-    static long timer1 , timer2 , timer3;
-    static Queue queue = new Queue();
+
     public static void main(String[] args) {
-        
-        
-        //read file data
-        //try {
-            //Scanner in = new Scanner(new FileInputStream("C:\\Users\\timot\\OneDrive\\Desktop\\um y2s2\\data structure\\Payment\\0.txt"));
-            
-            //while (in.hasNextLine()){
+        PriorityQueue<Transaction> q = new PriorityQueue<>();
+        String transaction;
+        String[] details;
+        long timer;
+        String id;
+        String tier;
+        int digit1=0;
+        long timer1;
+        int digit2;
+        long timer2;
+        int digit3=0;
 
-                Scanner in = new Scanner(System.in);
-                String data = in.nextLine();
-                if (data.equals("EXIT")){
-                    //break;
-                    System.exit(0);
-                }else if (data.equals("REBOOT")){
-                    queue.clear();
-                }else {
-                    if(queue.get(0)!=null && index3<index1){
-                        timer1 = queue.get(0).getEpochtime();
-                        index1 = (int) (timer1 % 10000 / 1000); 
-                    }
         
-                    queue.enqueue(processData(data));
-                    //System.out.println(time + " " + id + " " + tier);
-                    timer2 = processData(data).getEpochtime();
-                    index2 = (int) (timer2 % 10000 /1000);
-
-                    while(queue.getSize() ==1){
-                        timer1 = timer2;
-                        index1 = (int) (timer1 % 10000 / 1000); 
-                        break;
-                    }
-                    if(index2 > index1){
-                        queue.mergeSort();
-                        for (int x = 0 ; x < 100 ; x++){
-                            System.out.print(queue.dequeue().toString());                            
-                        }
+            Scanner in = new Scanner(System.in);
+            System.out.println("Please enter your transaction time, id and tier: ");
+            transaction = in.nextLine();
+            if (transaction.equals("EXIT")) {
+                Systen.exit(0);
+            }
+            details = transaction.split(" ");
+            timer = Long.parseLong(details[0]);
+            id = details[1];
+            tier = details[2];
+            Transaction t1 = new Transaction(timer, id, tier);
+            if(q.peek()!=null && digit3<digit1){
+                timer1 = q.peek().getTimer();
+                digit1 = (int) (timer1 % 10000 / 1000); 
+            }
+            q.offer(new Transaction(timer, id, tier));
+            timer2 = t1.getTimer();
+            digit2 = (int) (timer2 % 10000 / 1000);
+            while(q.size()==1){
+                timer1 = timer2;
+                digit1 = (int) (timer1 % 10000 / 1000); 
+                break;
+            } 
+            if(digit2 > digit1){
+                for(int i=0;i<100;i++){
+                    if(!q.isEmpty()){
+                        Transaction t = q.poll();
+                        System.out.print(t.getId() + " ");
+                }
+                }
                         System.out.println();
-                    }
-                    index1 = index3 = index2;
-                }
-                    
-            //}
-        //}
-
-                
             
+            }
+            digit1 = digit3 = digit2;
+       
             
-        // catch (FileNotFoundException e) {
-            //System.out.println("File not found");
-        //}
-    }
-
-    //classify data to epochTime , tier and transactionID
-    public static TransactionDetail processData(String data){
 
         
-        time = Long.parseLong(data.substring(0, 13));
-        id = data.substring(14, 46);
-        tier = data.substring(47);
-
-        trans = new TransactionDetail(time, id, tier);
-        return trans;
     }
-
-}
-
-
-class MergeSort {
-
-    static ArrayList<TransactionDetail> tmp;
-
-    public static void mergeSort(ArrayList<TransactionDetail> a,  int left, int right) {
-        int middle = (left + right) / 2;
-        if (left < right) {
-            mergeSort(a,  left, middle); //sort left half
-            mergeSort(a,  middle + 1, right); //sort right half
-            mergeSortedLists(a,  left, middle, right);
-        } // merge
-    }
-
-    public static void mergeSortedLists(ArrayList<TransactionDetail> a,  int left, int middle, int right) {
-        tmp = new ArrayList<TransactionDetail>();
-        int tempLeft = left;
-        int tempRight = middle + 1;
-        while (tempLeft <= middle && tempRight <= right)
-            if (a.get(tempLeft).getWaitingTime() <= (a.get(tempRight).getWaitingTime())) {
-                {
-                    tmp.add(a.get(tempLeft));
-                }
-                tempLeft++;
-            } else {
-                {
-                    tmp.add(a.get(tempRight));
-                }
-                tempRight++;
-            }
-
-        while (tempLeft <= middle) {
-            {
-                tmp.add(a.get(tempLeft));
-            }
-            tempLeft++;
-        }
-
-        while (tempRight <= right) {
-            {
-                tmp.add(a.get(tempRight));
-            }
-            tempRight++;
-        }
-        int i = left;
-        {
-            for (TransactionDetail value : tmp) {
-                a.set(i, value);
-                i++;
-            }
-        }
-    }
-    
-
-}
-
-class Queue {
-    ArrayList<TransactionDetail> priorityList;
-
-    public Queue() {
-        priorityList = new ArrayList<>();
-    }
-
-    public void enqueue(TransactionDetail elements){
-        if (priorityList.size() == 0 ){
-            priorityList.add(0,elements);;
-        }else {
-            priorityList.add(elements);
-        }
-    }
-
-    public void setPriorityList(ArrayList<TransactionDetail> list){
-        Collections.copy(priorityList, list);
-    }
-
-
-    public void clear() {
-        priorityList.clear();
-    }
-
-    public int getSize(){
-        return priorityList.size();
-    }
-
-    //public ArrayList<TransactionDetail> sort(){
-    //    MergeSort sort = new MergeSort(priorityList);
-    //    sort.divideArrayElements(0, priorityList.size()-1);
-    //    return sort.getArrayAfterSorting();
-    //}
-    public TransactionDetail dequeue(){
-        if (priorityList.isEmpty()) return null;
-        return priorityList.remove(0);
-        
-    }
-
-    public TransactionDetail get(int index){
-        if(priorityList.isEmpty()) return null;
-        return priorityList.get(index);
-    }
-
-
-    public void mergeSort (){
-        
-        MergeSort.mergeSort(priorityList, 0, priorityList.size()-1);
-    }
-
-    
-    
-
 
 
 }
 
 
-class TransactionDetail {
+class Transaction implements Comparable<Transaction>  {
+    long timer;
+    String id;
+    String tier;
+    Long stime;
 
-    private long epochTime;
-    private String tier;
-    private String transactionID;
-    private long waitingTime;
-
-    public TransactionDetail(long time, String ID, String tier) {
-        this.epochTime = time;
+    public Transaction(long timer, String id, String tier) {
+        this.timer = timer;
+        this.id = id;
         this.tier = tier;
-        this.transactionID = ID;
-        calculateWaitingTime(time);
     }
 
-
-    //maybe can try to cut to five digits then operation might be less
-    // To give the starting time of a transaction in a queue according to tier
-    public void calculateWaitingTime(long time) {
-
-        switch (this.tier) {
-            case "PLATINUM":
-                waitingTime = epochTime - 3000 ;
-                break;
-            case "GOLD":
-                waitingTime = epochTime - 2000 ;
-                break;
-            case "SILVER":
-                waitingTime = epochTime - 1000 ;
-                break;
-            case "BRONZE":
-                waitingTime = epochTime;
-                break;
-        }
-
+    public long getTimer() {
+        return timer;
     }
 
+    public String getId() {
+        return id;
+    }
     
-    public String getID() {
-        return this.transactionID;
+    public String getTier() {
+        return tier;
     }
 
-    public long getWaitingTime() {
-        return this.waitingTime;
+    public Long getStartingTime() {
+        switch(tier){
+        case "PLATINUM":
+            return stime = timer - 3000;
+        case "GOLD":
+            return stime = timer - 2000;
+        case "SILVER":
+            return stime = timer - 1000;
+        case "BRONZE":
+            return stime = timer - 0;
+        default:
+            break;
+        }
+        return stime;
+    }
+    
+    
+    @Override
+    public int compareTo(Transaction o1) {
+        return this.getStartingTime().compareTo(o1.getStartingTime());
     }
 
-    public void addWaitingTime(){
-        this.waitingTime += 1000;
-    }
-
-    public long getEpochtime(){
-        return this.epochTime;
-    }
-
+    @Override
     public String toString() {
-        return transactionID;
+        return id + " "  ;
     }
-
+    
+    
 }
-
-
