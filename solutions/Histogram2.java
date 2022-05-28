92ac9ff7aa05d42184ff4f1428dc148a544c705c
GOH QIAN XUAN <94182074+gohqianxuan@users.noreply.github.com>
2022-05-28 16:27:39 +0800

    Update Histogram2.java

--- solutions/Histogram2.java
+++ solutions/Histogram2.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Histogram {
+public class Histogram2 {
 
 	public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
