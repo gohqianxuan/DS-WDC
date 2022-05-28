5765d82aca8a6201a715258000ac4f083456312f
JS Low <63920059+lowjs@users.noreply.github.com>
2022-05-28 19:32:37 +0800

    Update Histogram.java

--- solutions/Histogram.java
+++ solutions/Histogram.java
@@ -3,39 +3,51 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Histogram {
+public class Histogram9 {
+    
     public static void main(String[] args) {
+    
     Scanner scanner = new Scanner(System.in);
+    
+    //input number of test case
     int numCase = scanner.nextInt();
                 
     for (int i = 0; i < numCase; i++) {
+       
+        //input number of data and bin
         int histogramSize = scanner.nextInt();
         int bin = scanner.nextInt();
         int temp;
+        
         List<Integer> histArr = new ArrayList<>();
         List<Integer> cutoffs = new ArrayList<>();
         List<Integer> counts = new ArrayList<>();
-
+        
+        //input data point
         for (int j = 0; j < histogramSize; j++) {
             temp = scanner.nextInt();
             histArr.add(temp);
         }
 		
+        //find required data
         int min = Collections.min(histArr);
         int max = Collections.max(histArr);
         int increment = (max - min) / bin;
+        
+        //calculate cutoffs value
         temp = min;
-            
         while (temp <= max) {
             cutoffs.add(temp);
             temp += increment;
         }
-            
+        
+        //sort data points 
         Collections.sort(histArr);
         temp = 1;
         int k = cutoffs.get(temp);
         int counter = 0;
                         
+        //calculate counts for each bin
         for (int j = 0; j < histogramSize; j++) {
             if (histArr.get(j) < k) 
                 counter++;
@@ -45,17 +57,18 @@ public class Histogram {
                     counts.add(++counter);
                     break;
                 }
-			
                 counts.add(counter);
                 counter = 1;
                 k = cutoffs.get(temp);
                 }
             }
-
+        
+        //print cutoffs of bin
         for (Integer each : cutoffs) 
             System.out.print(each + " ");
         System.out.println();
-            
+        
+        //print number of data for each bin
         for (Integer each : counts) 
             System.out.print(each + " ");
         System.out.println();
