fa5031e4d9d1f2945be3a2c075362a8400a684a1
GOH QIAN XUAN <94182074+gohqianxuan@users.noreply.github.com>
2022-05-29 00:23:02 +0800

    Add files via upload

--- solutions/Histogram.java
+++ solutions/Histogram.java
@@ -4,62 +4,74 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Histogram {
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int numCase = scanner.nextInt();
-
-        for (int i = 0; i < numCase; i++) {
-            int histogramSize = scanner.nextInt();
-            int bin = scanner.nextInt();
-            int temp;
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    //input number of test case
+    int numCase = scanner.nextInt();
                 
-            List<Integer> histArr = new ArrayList<>();
-            int[] cutoffs = new int[bin+1];
-            int[] counts = new int[bin];
-                
-            for (int j = 0; j < histogramSize; j++) 
-                histArr.add(scanner.nextInt());
-                
-            int min = Collections.min(histArr);
-            int max = Collections.max(histArr);
-            int increment = (max - min) / bin;
-                
-            temp = min;
-            int counter = 0;
-            while (temp <= max) {
-                cutoffs[counter] = temp;
-                temp += increment;
+    for (int i = 0; i < numCase; i++) {
+       
+        //input number of data and bin
+        int histogramSize = scanner.nextInt();
+        int bin = scanner.nextInt();
+        int temp;
+        
+        List<Integer> histArr = new ArrayList<>();
+        List<Integer> cutoffs = new ArrayList<>();
+        List<Integer> counts = new ArrayList<>();
+        
+        //input data point
+        for (int j = 0; j < histogramSize; j++) {
+            temp = scanner.nextInt();
+            histArr.add(temp);
+        }
+		
+        //find required data
+        int min = Collections.min(histArr);
+        int max = Collections.max(histArr);
+        int increment = (max - min) / bin;
+        
+        //calculate cutoffs value
+        temp = min;
+        while (temp <= max) {
+            cutoffs.add(temp);
+            temp += increment;
+        }
+        
+        //sort data points 
+        Collections.sort(histArr);
+        temp = 1;
+        int k = cutoffs.get(temp);
+        int counter = 0;
+                        
+        //calculate counts for each bin
+        for (int j = 0; j < histogramSize; j++) {
+            if (histArr.get(j) < k) 
                 counter++;
+            else {
+                temp++;
+                if (temp == cutoffs.size()) {
+                    counts.add(++counter);
+                    break;
                 }
-                
-            Collections.sort(histArr);
-            temp = 1;
-            int nextBin = cutoffs[temp];
-            counter = 0;
-                
-            for (int j = 0; j < histogramSize; j++) {
-                if (histArr.get(j) < nextBin)
-                    counter++;
-                else {
-                    temp++;
-                    if (temp == cutoffs.length) {
-                        counts[temp-2] = ++counter;
-                        break;
-                    }
-                    
-                    counts[temp-2] = counter;
-                    counter = 1;
-                    nextBin = cutoffs[temp];
+                counts.add(counter);
+                counter = 1;
+                k = cutoffs.get(temp);
                 }
             }
-
-            for (Integer each : cutoffs) 
-                System.out.print(each + " ");
-            System.out.println();
-                
-            for (Integer each : counts) 
-                System.out.print(each + " ");
-            System.out.println();
+        
+        //print cutoffs of bin
+        for (Integer each : cutoffs) 
+            System.out.print(each + " ");
+        System.out.println();
+        
+        //print number of data for each bin
+        for (Integer each : counts) 
+            System.out.print(each + " ");
+        System.out.println();
         }
     }
 }
