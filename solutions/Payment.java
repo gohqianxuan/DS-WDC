67a3d2e499713409bdf6210f7f51afe617df1f49
CopperCleric <92703867+CopperCleric@users.noreply.github.com>
2022-06-02 00:10:09 +0800

    Add files via upload

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,63 +1,64 @@
 import java.util.Scanner;
 import java.util.PriorityQueue;
-import java.util.InputMismatchException;
 public class Payment {
 
     public static void main(String[] args) {
         PriorityQueue<Transaction> q = new PriorityQueue<>();
         String transaction;
         String[] details;
-        long timer, timer1, timer2;
+        long timer, timer1 , timer2;
         String id, tier;
-        int digit1 = 0, digit3 = 0, digit2;
+        int digit1=0 , digit3 = 0,digit2;
 
         Scanner in = new Scanner(System.in);
-
-        while (in.hasNextLine()) {
+        transaction = in.nextLine();
+        while (transaction != null) {
             
-                transaction = in.nextLine();
-                if (transaction.equals("EXIT")) {
-                    break;
-                } else if (transaction.equals("REBOOT")) {
-                    q.clear();
-                }else {
-                    details = transaction.split(" ");
-                    timer = Long.parseLong(details[0]);
-                    id = details[1];
-                    tier = details[2];
-                    Transaction t1 = new Transaction(timer, id, tier);
-                    if (q.peek() != null && digit3 < digit1) {
-                        timer1 = q.peek().getTimer();
-                        digit1 = (int) (timer1 % 10000 / 1000);
-                    }
-                    q.offer(new Transaction(timer, id, tier));
-                    timer2 = t1.getTimer();
-                    digit2 = (int) (timer2 % 10000 / 1000);
-                    while (q.size() == 1) {
-                        timer1 = timer2;
-                        digit1 = (int) (timer1 % 10000 / 1000);
-                        break;
-                    }
-                    if (digit2 > digit1) {
-                        for (int i = 0; i < 100; i++) {
-                            if (!q.isEmpty()) {
-                                Transaction t = q.poll();
-                                System.out.print(t + " ");
-                            }
-                        }
-                        System.out.println();
-    
-                    }
-                    digit1 = digit3 = digit2;
+            if (transaction.equals("EXIT")) {
+                break;
+            }else if(transaction.equals("REBOOT")){
+                q.clear();
+            }else {
+                details = transaction.split(" ");
+                timer = Long.parseLong(details[0]);
+                id = details[1];
+                tier = details[2];
+                Transaction t1 = new Transaction(timer, id, tier);
+                if(q.peek()!=null && digit3<digit1){
+                    timer1 = q.peek().getTimer();
+                    digit1 = (int) (timer1 % 10000 / 1000); 
                 }
-            } 
+                q.offer(new Transaction(timer, id, tier));
+                timer2 = t1.getTimer();
+                digit2 = (int) (timer2 % 10000 / 1000);
+                while(q.size()==1){
+                    timer1 = timer2;
+                    digit1 = (int) (timer1 % 10000 / 1000); 
+                    break;
+                } 
+                if(digit2 > digit1){
+                    for(int i=0;i<100;i++){
+                        if(!q.isEmpty()){
+                            Transaction t = q.poll();
+                            System.out.print(t + " ");
+                    }
+                }
+                    System.out.println();
+            
+            }
+            digit1 = digit3 = digit2;
+            }
+            
+       
+            
 
-        
+        }
     }
 
 }
 
-class Transaction implements Comparable<Transaction> {
+
+class Transaction implements Comparable<Transaction>  {
     long timer;
     String id;
     String tier;
@@ -76,27 +77,28 @@ class Transaction implements Comparable<Transaction> {
     public String getId() {
         return id;
     }
-
+    
     public String getTier() {
         return tier;
     }
 
     public Long getStartingTime() {
-        switch (tier) {
-            case "PLATINUM":
-                return stime = timer - 3000;
-            case "GOLD":
-                return stime = timer - 2000;
-            case "SILVER":
-                return stime = timer - 1000;
-            case "BRONZE":
-                return stime = timer;
-            default:
-                break;
+        switch(tier){
+        case "PLATINUM":
+            return stime = timer - 3000;
+        case "GOLD":
+            return stime = timer - 2000;
+        case "SILVER":
+            return stime = timer - 1000;
+        case "BRONZE":
+            return stime = timer;
+        default:
+            break;
         }
         return stime;
     }
-
+    
+    
     @Override
     public int compareTo(Transaction o1) {
         return this.getStartingTime().compareTo(o1.getStartingTime());
@@ -104,7 +106,8 @@ class Transaction implements Comparable<Transaction> {
 
     @Override
     public String toString() {
-        return id + " ";
+        return id + " "  ;
     }
-
+    
+    
 }
