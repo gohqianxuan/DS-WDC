9aed84c7a98bc7e2c9551177e14da35465ba9be5
JS Low <63920059+lowjs@users.noreply.github.com>
2022-06-22 01:32:17 +0800

    Create run.sh

---
+++ tasks/popular/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+VERBOSE=true
+TIMELIMIT=5s
+mkfifo iopipe0 iopipe1
+
+if [[ $VERBOSE ]]
+then
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions Popular | tee iopipe1 & <iopipe1 python3 tasks/popular/run.py | tee iopipe0;
+else
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions Popular | python3 tasks/popular/run.py > iopipe0;
+fi
+PASS=$?
+
+rm iopipe0 iopipe1
+exit $PASS
